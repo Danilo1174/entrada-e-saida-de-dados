98def2fea2785dcda726b1edcd0362d307987064
Danilo Alves <daniloalves.1996@gmail.com>
2022-12-02 21:19:14 -0300

    entrada e saida de dados

---
+++ Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+namespace Lista_de_Exercicios
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            // Exercicio 1
+
+            // int num1, num2, soma;
+
+            //  Console.WriteLine(" escreva o primeiro valor a somar : ");
+            //  num1 = int.Parse (Console.ReadLine());
+            //  Console.WriteLine(" escreva o segundo  valor a somar : ");
+            // num2 = int.Parse(Console.ReadLine());
+
+            // soma = num1 + num2;
+
+            // Console.WriteLine(" o resultado da soma é: " +  soma);
+
+            //--------------------------------------------------------------------------------------
+
+            // Exercicio 2
+
+
+            // double pi = 3.14159, r;
+
+            // Console.WriteLine(" digite o valor do raio para calcular a area: ");
+            //r = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Area = " + { (pi * (r * r)).ToString("0.0000")});
+
+
+            //----------------------------------------------------------------------------------------
+
+            // Exercicio 3
+
+            //int A, B, C, D, dif;
+
+
+            //  Console.WriteLine("Digite o valor de A: ");
+            // A = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Digite o valor de B: ");
+            // B = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Digite o valor de C: ");
+            // C = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Digite o valor de D: ");
+            //  D = int.Parse(Console.ReadLine());
+
+            //dif = (A * B - C * D);
+
+            // Console.WriteLine(" a diferenca é: " + dif);
+
+
+            //----------------------------------------------------------------------------------------
+
+            // Exercicio 4
+
+            // double NF, NH, HT;
+
+            // Console.WriteLine(" digite o número do funcionario: ");
+
+            // NF = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(" digite o número de horas trabalhadas : ");
+
+            // NH = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("digite o valor que  funcionario recebe por hora trabalhada: ");
+
+            // HT = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("número do funcionario:" + NF);
+            // Console.WriteLine("salário : R$ "  + (NH* HT).ToString("0.00"));
+
+
+
+
+            //----------------------------------------------------------------------------------------
+
+
+            // Exercicio 5
+
+            //  double A, B, C, pi = 3.14159;
+
+
+            // Console.WriteLine("digite o valor de A:");
+            // A = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("digite o valor de B:");
+            // B = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("digite o valor de C:");
+            // C = double.Parse(Console.ReadLine());
+
+
+
+            // Console.WriteLine("A área do triângulo retângulo é: " + ((A * C) / 2).ToString("0.000"));
+            // Console.WriteLine("A área do Circulo  é:" + (pi * (C * C)).ToString("0.000"));
+            // Console.WriteLine("A área do trapézio é:" + ((A + B) * C / 2).ToString("0.000"));
+            // Console.WriteLine("A área do quadrado é : " + (B * B).ToString("0.000"));
+            // Console.WriteLine("e área do retângulo é: " + (A * B).ToString("0.000"));
+
+
+
+
+            //----------------------------------------------------------------------------------------
+            //----------------------------------------------------------------------------------------
+
+
+            // Exercicio 1
+
+            //int a, b;
+
+            //Console.WriteLine ("digite o valor de a: ");
+            //a = int.Parse(Console.ReadLine  ());
+
+            //Console.WriteLine("digite o valor de b : ");
+            //b = int.Parse(Console.ReadLine  ());
+
+            //if (a % b == 0 || b % a == 0)
+            //{
+            //  Console.WriteLine ("são multiplos");
+            //}
+            //else
+            //{
+            //  Console.WriteLine("não são multiplos");
+            //}
+
+
+
+            //----------------------------------------------------------------------------------------
+
+
+            // Exercicio 2
+
+            //int codigo, quantidade;
+            // double preco = 0.0;
+
+            // Console.WriteLine("codigo   | especificacao   |   preco ");
+            // Console.WriteLine("  1      | cachorro quente |   r$4.00");
+            // Console.WriteLine("  2      | x-salada        |   r$4.50");
+            // Console.WriteLine("  3      | x-bacon         |   r$5.00");
+            // Console.WriteLine("  4      | bauru           |   r$2.50");
+            // Console.WriteLine("  5      | refrigerante    |   r$1.50");
+
+
+
+            //  Console.WriteLine("digite o código: ");
+            // codigo = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("digite a quantidade: ");
+            //  quantidade = int.Parse(Console.ReadLine());
+
+            // if (codigo == 1)
+            // {
+            //     preco = 4.00 * quantidade;
+            // }
+            // else if (codigo == 2)
+            // {
+            //     preco = 4.50 * quantidade;
+            //  }
+            // else if (codigo == 3)
+            // {
+            //     preco = 5.00 * quantidade;
+            //  }
+            // else if (codigo == 4)
+            //  {
+            //     preco = 2.00 * quantidade;
+            //  }
+            // else if (codigo == 5)
+            //  {
+            //     preco = 1.50 * quantidade;
+            // }
+
+            // Console.WriteLine("total: R${0:f2}", preco);
+
+
+
+            //----------------------------------------------------------------------------------------
+
+            // Exercicio 3
+
+
+           // Console.WriteLine("digite o valor de x: ");
+            //double x = double.Parse(Console.ReadLine());
+
+           // Console.WriteLine ("digite o valor de y:");
+            //double y = double.Parse(Console.ReadLine());
+
+
+          //  if (x == 0.0 && y == 0.0)
+            //{
+              //  Console.WriteLine("origem");
+            //}
+            //else if (x == 0.0)
+            //{
+              //  Console.WriteLine("eixo y");
+            //}
+            //else if (y == 0.0)
+            //{
+              //  Console.WriteLine("eixo x");
+            //}
+            //else if (x > 0.0 && y > 0.0)
+            //{
+              //  Console.WriteLine("q1");
+            //}
+            //else if (x < 0.0 && y > 0.0)
+            //{
+              //  Console.WriteLine("q2");
+            //}
+           // else if (x < 0.0 && y < 0.0)
+            //{
+              //  Console.WriteLine("q3");
+            //}
+           // else
+            //{
+              //  Console.WriteLine("q4");
+            //}
+
+
+
+
+
+        }
+    }
+}
